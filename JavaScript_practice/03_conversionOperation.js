74615d92801d4c5a67e674454734789763ee874f
pragatikushwah <pragatikushwah13@gmail.com>
2025-04-02 11:42:36 +0530

    convertion operations

--- JavaScript_practice/03_conversionOperation.js
+++ JavaScript_practice/03_conversionOperation.js
@@ -25,3 +25,33 @@ let soeNumber=33
 let  stringNumber=String(soeNumber)
 console.log(stringNumber);//33
 console.log(typeof stringNumber)//string
+
+
+//************************Operation***************************** */
+let value =3;
+let negvalue =-value
+console.log(negvalue)//-3
+
+console.log(2**3)//8
+
+let str1="hello"
+let str2=" anshul"
+let str3=str1+str2
+console.log(str3)//hello anshul
+
+console.log("1"+2)//12
+console.log(1+"2")//12
+console.log("1"+2+2)//122
+console.log(1+2+"2")//32
+console.log(+true)//1
+console.log(+"")//0
+let num1, num2, num3
+num1=num2=num3=2+2
+console.log(num1)//4
+
+let gamecounter=100
+gamecounter++;
+console.log(gamecounter)//101
+++gamecounter
+console.log(gamecounter++)//102
+console.log(gamecounter)//103
